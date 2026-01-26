391bf85357182b0c99a9b11638bf7016a287be42
StudTi <d.c.0303@mail.ru>
2026-01-26 19:12:01 +0500

    День: 4 Успех!

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @tipescript-eslint/no-unsafe-call*/
+/* eslint-disable @tipescript-eslint/no-unsafe-assigment*/
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
+import { PrismaClient } from "prisma/generated/prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly pool: Pool
+
+  constructor() {
+    const connectionString = process.env.DATABASE_URL
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined')
+    }
+
+    const pool = new Pool({ connectionString })
+    const adapter = new PrismaPg(pool)
+
+    super({ adapter })
+    this.pool = pool
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+    console.log('Prisma connected to PosthreSQL')
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+    await this.pool.end()
+    console.log('Prisma disconnected from PostgreSQL')
+  }
+}
+
+
+
+
+
+
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+
+@Resolver()
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => String)
+  getProfile(){
+    return 'Joe Doe'
+  }
+}
+
